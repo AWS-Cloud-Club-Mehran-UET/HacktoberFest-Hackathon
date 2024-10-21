da4bf318f0894864a0e56fb196f94e0522b7d805
UmairRaaz <umairgopang123@gmail.com>
2024-10-21 16:14:12 +0500

    fix issue 09: complete project

--- umair-09/src/app/auth/auth.config.js
+++
@@ -1,7 +0,0 @@
-export const authconfig = {
-    session: {
-        strategy: "jwt",
-        maxAge: 86400,
-    },
-    providers: []
-}
--- umair-09/src/models/onboardingtask.models.js
+++
@@ -1,9 +0,0 @@
-import mongoose, { Schema } from "mongoose";
-
-const onBoardingTaskSchema = new mongoose.Schema({
-   employee :{type : Schema.Types.ObjectId, ref: "EmployeeSchema", required: true },
-   depatment : {type : Schema.Types.ObjectId, ref: "DepartmentSchema", required: true },
- 
-}, {timestamps: true} )
-
-export const onBoardingTask = mongoose.model.onBoardingTask || mongoose.models("onBoardingTaskSchema")
