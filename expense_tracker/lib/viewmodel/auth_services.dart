c0a90c510c4a320f8722b68528941ab502f9b4b2
Hammad Ahmed <hammadak350@gmail.com>
2024-10-21 16:05:58 +0500

    fixed issue

--- expense_tracker/lib/viewmodel/auth_services.dart
+++ expense_tracker/lib/viewmodel/auth_services.dart
@@ -1,1 +1,39 @@
+import 'package:firebase_auth/firebase_auth.dart';
 
+class AuthMethods {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  Future<void> signUp(String email, String password) async {
+    try {
+      await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> signIn(String email, String password) async {
+    try {
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    }
+  }
+
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
+
+  // Future<User?> getCurrentUser() async {
+  //   return _auth.currentUser;
+  // }
+}
