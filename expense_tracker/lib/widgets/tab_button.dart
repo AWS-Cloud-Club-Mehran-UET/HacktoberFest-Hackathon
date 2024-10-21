d0251430f0169af8605c1c7bd5e4f0fbacd1535f
Hammad Ahmed <hammadak350@gmail.com>
2024-10-21 12:53:18 +0500

    added custom widgets

--- expense_tracker/lib/widgets/tab_button.dart
+++ expense_tracker/lib/widgets/tab_button.dart
@@ -2,21 +2,25 @@ import 'package:flutter/material.dart';
 
 class TabButton extends StatelessWidget {
   final String buttonText;
-  const TabButton({required this.buttonText, super.key});
+  final VoidCallback onTap;
+  const TabButton({required this.onTap, required this.buttonText, super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: MediaQuery.of(context).size.height * 0.045,
-      width: MediaQuery.of(context).size.width * 0.3,
-      decoration: BoxDecoration(
-        color: Colors.black,
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Center(
-        child: Text(
-          buttonText,
-          style: const TextStyle(color: Colors.white),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: MediaQuery.of(context).size.height * 0.045,
+        width: MediaQuery.of(context).size.width * 0.3,
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Center(
+          child: Text(
+            buttonText,
+            style: const TextStyle(color: Colors.white),
+          ),
         ),
       ),
     );
