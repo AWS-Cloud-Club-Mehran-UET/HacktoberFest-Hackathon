2f614ddb0ba8235639ddc9829a3b12c489b8c042
Hammad Ahmed <hammadak350@gmail.com>
2024-10-21 12:53:18 +0500

    Added firestore methods

--- expense_tracker/lib/view/home_screen.dart
+++ expense_tracker/lib/view/home_screen.dart
@@ -1,19 +1,62 @@
 import 'package:expense_tracker/widgets/expense_card.dart';
+import 'package:expense_tracker/widgets/monthly_expenses.dart';
+import 'package:expense_tracker/widgets/recent_expenses.dart';
 import 'package:expense_tracker/widgets/tab_button.dart';
 import 'package:expense_tracker/widgets/transaction_tile.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
+    var tabIndex = 0;
+    var standartCurrency = 'PKR';
+    var currencyList = [
+      'USD',
+      'EUR',
+      'PKR',
+    ];
+
+    changeIndex() {
+      setState(() {
+        if (tabIndex == 0) {
+          tabIndex = 1;
+        } else {
+          tabIndex = 0;
+        }
+      });
+    }
+
+    List<Widget> widgets = [MonthlyExpenses(), RecentExpenses()];
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 218, 214, 214),
       appBar: AppBar(
         title: const Text('Dashboard'),
         centerTitle: true,
         backgroundColor: const Color.fromARGB(255, 218, 214, 214),
+        actions: [
+          DropdownButton(
+            items: currencyList.map((String value) {
+              return DropdownMenuItem(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+            onChanged: (value) {
+              standartCurrency = value!;
+            },
+          ),
+          const Icon(Icons.currency_exchange),
+          const SizedBox(
+            width: 5,
+          ),
+        ],
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
@@ -46,23 +89,30 @@ class HomeScreen extends StatelessWidget {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                TabButton(buttonText: 'Recent'),
-                TabButton(buttonText: 'Monthly'),
+                // TabButton(
+                //   buttonText: 'Recent',
+                //   onTap: () {
+                //     setState(() {
+                //       tabIndex = 0;
+                //     });
+                //   },
+                // ),
+                // TabButton(
+                //   buttonText: 'Monthly',
+                //   onTap: changeIndex,
+                // ),
+                ElevatedButton(
+                  onPressed: changeIndex(),
+                  child: Text('Recent'),
+                ),
+                ElevatedButton(
+                  onPressed: changeIndex(),
+                  child: Text('Monthly'),
+                ),
               ],
             ),
           ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: 10,
-              itemBuilder: (context, index) {
-                return TransactionTile(
-                  leadingIcon: Icon(Icons.arrow_upward),
-                  amount: '150000',
-                  transactionType: 'Income',
-                );
-              },
-            ),
-          ),
+          Expanded(child: widgets[tabIndex]),
         ],
       ),
     );
