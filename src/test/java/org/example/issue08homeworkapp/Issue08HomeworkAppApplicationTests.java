26817a445fd14c97bd6b7168752f4779dc86b4ad
Hamza Ali <saira21perveen@gmail.com>
2024-10-21 10:06:37 +0500

    Redundent First Commit v1.0

---
+++ src/test/java/org/example/issue08homeworkapp/Issue08HomeworkAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.issue08homeworkapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Issue08HomeworkAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
